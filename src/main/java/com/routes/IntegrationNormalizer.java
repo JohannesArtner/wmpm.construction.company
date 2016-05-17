dc7c34cb0cb76e9a5a4ec55da719d89c85840b25
daniel <Daniel.Wailzer@gmx.at>
2016-05-17 19:40:45 +0200

    path change

--- src/main/java/com/routes/IntegrationNormalizer.java
+++ src/main/java/com/routes/IntegrationNormalizer.java
@@ -9,12 +9,12 @@ import org.apache.camel.builder.RouteBuilder;
 public class IntegrationNormalizer extends RouteBuilder {
     public void configure() throws Exception {
         // we need to normalize two types of incoming messages
-    from("direct:start")
+        from("imap://admin@mymailserver.com?password=secret&unseen=true&consumer.delay=60000")
     .process(new LoggingProcessor())
     .choice()
     .when().xpath("/employee").to("bean:normalizer?method=employeeToPerson")
     .when().xpath("/customer").to("bean:normalizer?method=customerToPerson")
     .end()
-    .to("mock:result");
+    .to("file:target/inbox");
     }
 }
