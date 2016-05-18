1686e2d30af84c1f07060f31ac0b0e29e9cced1b
daniel <Daniel.Wailzer@gmx.at>
2016-05-18 17:31:43 +0200

    Rejected commit: Default

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,10 +1,23 @@
 package com;
-import model.ClientRequest;
+//import model.ClientRequest;
+import com.TestBean.TestBean;
+import com.services.ClientRequestService;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Endpoint;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.log.LogComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.util.jndi.JndiContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import services.ClientRequestService;
+//import services.ClientRequestService;
+import com.model.*;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.routes.*;
 
 import java.util.Date;
 
@@ -22,10 +35,25 @@ public class Application implements CommandLineRunner {
         SpringApplication.run(Application.class);
     }
 
-    @Override
-    public void run(String ... args){
-        ClientRequest request = new ClientRequest("Max", "Mustermann", "max@muster.at", "Musterstraße 18/2, 1010 Wien", "Ich will ein neues Haus bauen!", new Date(), new Date());
+    private CamelContext camel;
 
-        clientRequestService.createNewRequest(request);
+    @Override
+    public void run(String ... args)throws Exception {
+       // ClientRequest request = new ClientRequest("Max", "Mustermann", "max@muster.at", "Musterstraße 18/2, 1010 Wien", "Ich will ein neues Haus bauen!", new Date(), new Date());
+
+        // create the camel context that is the "heart" of Camel
+        camel = new DefaultCamelContext();
+        camel.addRoutes(new IntegrationNormalizer());
+        camel.addRoutes(new WireTapNewClients());
+        camel.addRoutes(new ConstructionDecision());
+
+        //camel.addRoutes(new IntegrationRoute());
+        camel.start();
+        Thread.sleep(30000);
+        camel.stop();
+
+
+       // clientRequestService.createNewRequest(request);
     }
+
 }
