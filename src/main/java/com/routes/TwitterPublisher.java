1bb96879a2bfe7dabb655e1f2f8c81184c2c80b9
Maximilian Mueller <maximilian.mueller@hotmail.com>
2016-05-21 14:05:30 +0200

    Created Facebook and Twitter classes

---
+++ src/main/java/com/routes/TwitterPublisher.java
@@ -0,0 +1,7 @@
+package com.routes;
+
+/**
+ * Created by maxmueller on 21.05.16.
+ */
+public class TwitterPublisher {
+}
---
+++ src/test/java/FacebookTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by maxmueller on 21.05.16.
+ */
+public class FacebookTest {
+}
