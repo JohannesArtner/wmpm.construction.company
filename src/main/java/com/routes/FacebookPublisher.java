e22a3324543aed8b1f101270a30dfb1e1e3e8b9c
Maximilian Mueller <maximilian.mueller@hotmail.com>
2016-05-22 09:34:14 +0200

    Twitter

--- src/main/java/com/routes/FacebookPublisher.java
+++ src/main/java/com/routes/FacebookPublisher.java
@@ -8,16 +8,17 @@ public class FacebookPublisher extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("direct:start")
 
                 //https://graph.facebook.com/oauth/access_token?client_id = YOUR_APP_ID & client_secret =YOUR_APP_SECRET&grant_type = client_credentials
                 //.setHeader("CamelHttpUri", constant("https://graph.facebook.com/endpoint?key=value&amp;access_token=1556856541283678|6b5f95106e108e957bf7f2c42b4bd3a9"))
                 //.to("https://www.facebook.com")
 
-                .to("facebook://endpoint[?options]")
-                .convertBodyTo(String.class)
-                .to("log:FacebookDump")
-                .to("mock:finish");
+        from("direct:foo")
+                        .to("facebook://postFeed?inBody=postUpdate")
+                        .to("facebook://endpoint[?options]")
+                        .convertBodyTo(String.class)
+                        .to("log:FacebookDump")
+                        .to("mock:finish");
 
         ;
     }
