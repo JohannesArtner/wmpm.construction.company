6f9b787f50b71a37499f099ecd7abafb9a62a935
Johannes <Johannes.Artner@gmx.at>
2016-05-23 19:55:34 +0200

    added a route for processing an Offer

---
+++ src/main/java/com/routes/requestProcessor/ConstructionDecision.java
@@ -0,0 +1,36 @@
+package com.routes.requestProcessor;
+
+import com.services.RequestService;
+import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by VanHelsing on 17.05.2016.
+ */
+
+@Component
+public class ConstructionDecision extends RouteBuilder {
+    private static final Logger LOG = LoggerFactory.getLogger(ProcessRequest.class);
+
+    @Autowired
+    RequestService requestService;
+
+    @Autowired
+    ProcessRequest processor;
+
+    @Override
+    public void configure() throws Exception {
+
+        from("direct:processRequest").process(processor).log("Request received!")
+                .choice()
+                .when(body().contains("Tiefbau"))
+                .to("bean:kostenvoranschlagsService?method=makeForTiefbau(${body})")
+                .when(body().contains("Hochbau"))
+                .to("bean:kostenvoranschlagsService?method=makeForHochbau(${body})")
+                .otherwise()
+                .to("mock:others");
+    }
+}
