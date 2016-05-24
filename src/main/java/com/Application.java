68777ecd4b290b5c7a93ce20f41a0b6382e997e0
Rudolf <rudolf@plettenberg.at>
2016-05-24 13:56:44 +0200

    Small changes + application.properties BUGFIX: now not excluding EMployeeDB anymore

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,11 +1,19 @@
 package com;
+import com.database.employeeDB.EmployeeDAO;
+import com.database.employeeDB.model.Employee;
+import com.database.employeeDB.model.ProjectManager;
+import com.database.employeeDB.model.SpecializationType;
+import com.database.projectDB.RequestDAO;
 import com.database.projectDB.model.Request;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.services.RequestService;
 import org.apache.camel.*;
+
+import java.util.Date;
 
 /**
  * Created by mionisation on 4/27/16.
@@ -14,10 +22,16 @@ import org.apache.camel.*;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
+    static Logger logger = Logger.getLogger(Application.class.getName());
+
+
     @Autowired
-    private RequestService clientRequestService;
+    private RequestDAO requestDAO;
+    @Autowired
+    private EmployeeDAO employeeDAO;
 
     public static void main(String[] args) {
+        logger.info("----------STARTING APPLICATION----------");
         SpringApplication.run(Application.class);
     }
 
@@ -26,6 +40,19 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String ... args) throws Exception {
+
+        logger.info("MAKING NEW REQUEST");
+        Request r = new Request();
+        r.setDateFrom(new Date());
+        r.setDateTo(new Date());
+        r.setSpecializationType(SpecializationType.HOCHBAU);
+        r.setLocation("Location");
+        r.setSquaremeters(400d);
+        r.setDescription("Description");
+
+        requestDAO.save(r);
+        logger.info("NEW REQUEST SAVED");
+
 
         try {
             ProducerTemplate template = camelContext.createProducerTemplate();
