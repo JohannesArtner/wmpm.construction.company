553d512c02d5107f15b05698e764ecd854dbf23a
Michael Ion <michael.antonius.ion@gmail.com>
2016-06-09 11:28:11 +0200

    Introduce GoogleCalenarRoute

--- src/main/java/com/routes/offerProcessor/routes/CreateGoogleCalendarRoute.java
+++ src/main/java/com/routes/offerProcessor/routes/CreateGoogleCalendarRoute.java
@@ -1,7 +1,18 @@
 package com.routes.offerProcessor.routes;
 
+import org.apache.camel.builder.RouteBuilder;
+import com.google.api.services.calendar.model.Calendar;
 /**
  * Created by mionisation on 6/9/16.
  */
-public class CreateGoogleCalendarRoute {
+public class CreateGoogleCalendarRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        Calendar cal = new Calendar();
+        from("direct:createGoogleCalendar")
+                //TODO: authentification
+                // TODO acl or calendars endpoint?,
+                // TODO insert or update (which endpoint) new dates with com.google.api.services.calendar.model.Calendar
+        .to("google-calendar://calendars/update?calendarId=mycalendar&content=GoogleCalendar");
+    }
 }
