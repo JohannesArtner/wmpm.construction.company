7422309637094ef5b13f53c62d4f4c8d1a5c6fd9
Johannes <Johannes.Artner@gmx.at>
2016-06-14 11:55:21 +0200

    worked on sending email with the offer as pdf attachment

--- src/main/java/com/routes/requestProcessor/routes/SendMail.java
+++ src/main/java/com/routes/requestProcessor/routes/SendMail.java
@@ -27,42 +27,54 @@ public class SendMail extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        Endpoint endpoint = context.getEndpoint("smtp://smtp.gmail.com?port=587&username=offerManagementConstructionCom@gmail.com&password=supersafepw&mail.smtp.auth=true&mail.smtp.starttls.enable=true");
-        endpoint.start();
-
-
         log.info("Initializing Email Service");
 
-        Properties props = new Properties();
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.googlemail.com");
-        props.put("mail.smtp.port", "587");
-        Authenticator auth = null;
-        props.put("mail.smtp.auth", "true");
-        auth = new Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("offerManagementConstructionCom@gmail.com", "supersafepw");
-            }
-        };
-        Session t = Session.getInstance(props, auth);
+        Endpoint endpointEmail = context.getEndpoint("smtp://smtp.gmail.com?port=587&username=offerManagementConstructionCom@gmail.com&password=supersafepw&mail.smtp.auth=true&mail.smtp.starttls.enable=true");
+        endpointEmail.start();
+
+        String path = "C:/Temp/camel/offerToPdf";
+
+        Endpoint endpointFile = context.getEndpoint("file://" + path + "?noop=true&move=done/${file:name}");
+        endpointFile.start();
+
+        PollingConsumer consumer = endpointFile.createPollingConsumer();
 
 
-        ProducerTemplate template = context.createProducerTemplate();
-        template.start();
-        Exchange exchange = endpoint.createExchange();
-        Message in = exchange.getIn();
-        String body = "Hello Customer!\nHere is your offer.\nIt is a good one!\n\nRegards The constructors.";
-        in.setBody(body);
-        in.addAttachment("offer.pdf", new DataHandler(new FileDataSource("c:/Temp/camel/offerToPdf/offer.pdf")));
+        while(true) {
+            consumer.start();
+            Exchange exchangeFile = consumer.receive();
 
-        Map<String, Object> headers = null;
-        headers = new HashMap<String, Object>();
-        headers.put("Subject", "This is your special Offer!");
-        headers.put("To", "johannes.artner@gmx.at");
-        headers.put("From", "offerManagementConstructionCom@gmail.com");
-        template.sendBodyAndHeaders(endpoint, body, headers);
+            Properties props = new Properties();
+            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", "smtp.googlemail.com");
+            props.put("mail.smtp.port", "587");
+            Authenticator auth = null;
+            props.put("mail.smtp.auth", "true");
+            auth = new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication("offerManagementConstructionCom@gmail.com", "supersafepw");
+                }
+            };
+            Session t = Session.getInstance(props, auth);
+
+            ProducerTemplate template = context.createProducerTemplate();
+            template.start();
+            Exchange exchange = endpointEmail.createExchange();
+            Message in = exchange.getIn();
+            String body = "Hello Customer!\nHere is your offer.\nIt is a good one!\n\nRegards The constructors.";
+            in.setBody(body);
+            in.addAttachment("offer.pdf", new DataHandler(new FileDataSource("c:/Temp/camel/offerToPdf/offer.pdf")));
+
+            Map<String, Object> headers = null;
+            headers = new HashMap<String, Object>();
+            headers.put("Subject", "This is your special Offer!");
+            headers.put("To", "johannes.artner@gmx.at");
+            headers.put("From", "offerManagementConstructionCom@gmail.com");
+            template.sendBodyAndHeaders(endpointEmail, body, headers);
+            consumer.stop();
+        }
     }
 
     public static void main(String[] args){
