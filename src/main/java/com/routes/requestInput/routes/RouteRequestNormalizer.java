f73b938b06e03425ad81955dc27ad8ee7d12de35
daniel <Daniel.Wailzer@gmx.at>
2016-06-19 16:08:15 +0200

    Errorhandling when nullpointer

--- src/main/java/com/routes/requestInput/routes/RouteRequestNormalizer.java
+++ src/main/java/com/routes/requestInput/routes/RouteRequestNormalizer.java
@@ -1,6 +1,7 @@
 package com.routes.requestInput.routes;
 
 import com.routes.requestInput.processor.LoggerProcessor;
+import com.routes.requestInput.processor.ReouteRequestNormalizerFailureHandler;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
@@ -16,7 +17,9 @@ public class RouteRequestNormalizer extends AbstractRestRouteBuilder {
     @Override
     public void configure() throws Exception {
         logger.info("Route from Normalizer to Database");
+        onException(NullPointerException.class).process(new ReouteRequestNormalizerFailureHandler()).stop();
         from("seda:requestNormalizerQueue")
+                .transacted()
                 .log("Starting normalization")
                 .choice()
                 .when(header("origin").isEqualTo("form"))
