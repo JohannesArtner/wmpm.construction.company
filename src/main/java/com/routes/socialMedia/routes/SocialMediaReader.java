452c9374958f8a9a59f2fffdad2e3b03f8e4c99b
Maximilian Mueller <maximilian.mueller@hotmail.com>
2016-06-21 16:56:32 +0200

    FB Token

--- src/main/java/com/routes/socialMedia/routes/SocialMediaReader.java
+++ src/main/java/com/routes/socialMedia/routes/SocialMediaReader.java
@@ -13,7 +13,7 @@ public class SocialMediaReader extends RouteBuilder {
         /** Permissions denied. App has to be reviewed by Facebook in order to use the permission "user_posts" to read the posts on a users timeline **/
         /** To show that the Facebook route is established: Public information can be retrieved but not more until reviewing**/
 
-        from("facebook://me?oAuthAppId=1556856541283678&oAuthAppSecret=6b5f95106e108e957bf7f2c42b4bd3a9&oAuthAccessToken=EAAWH8ZBkcBV4BAHlZBFbZBoee5tAcgSVCHD5DJnwrJ54eIcxa9ELmWMiRI3b4QXTf0PXE7Ft2nZBFPP1L3hRA5YFQwhc3DyoS96h8SjgYH2lcMk29VMZAk3Yg0WxtPDGEtZBAZBMSgSL66akOLkes7OZBXNYEPNKECCuAXrrEktWuQZDZD&consumer.delay=100000")
+        from("facebook://me?oAuthAppId=1556856541283678&oAuthAppSecret=6b5f95106e108e957bf7f2c42b4bd3a9&oAuthAccessToken=EAAWH8ZBkcBV4BACUV6tvjqQo7JxNQDGIwzwPxyMjliCNTrPZB3L38ZBUn3fYlH9GPQf1WTEsgatg1yuSHB2M7gGsgNggOCfUwKQatM119yS74HZBc9Yua9q87wvhS7us8U9ZCWlmOZAxR9ohN3gNnh5gZBAg01FLeULPRLZAwrSHEwZDZD&consumer.delay=100000")
                 .log(LoggingLevel.INFO, "SocialMediaReader", "FACEBOOK Info: ${body}")
                 .to("mock:bean:test");
 
